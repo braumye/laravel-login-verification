86c7d8037f12bd907488f9048fa101d7985c3968
braumye <braum.dev@gmail.com>
2020-11-09 14:35:56 +0800

    wip

--- src/LoginVerificationServiceProvider.php
+++ src/LoginVerificationServiceProvider.php
@@ -10,78 +10,78 @@ use Illuminate\Support\ServiceProvider;
 
 class LoginVerificationServiceProvider extends ServiceProvider
 {
-    // public function register()
-    // {
-    //     $this->mergeConfigFrom(__DIR__.'/../config/login-verification.php', 'login-verification');
-    // }
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/login-verification.php', 'login-verification');
+    }
 
-    // public function boot(Filesystem $filesystem)
-    // {
-    //     $this->publishConfig();
-    //     $this->publishMigrations($filesystem);
-    //     $this->publishTranslations();
-    //     $this->registerRoutes();
-    //     $this->registerResources();
-    // }
+    public function boot(Filesystem $filesystem)
+    {
+        $this->publishConfig();
+        $this->publishMigrations($filesystem);
+        $this->publishTranslations();
+        $this->registerRoutes();
+        $this->registerResources();
+    }
 
-    // protected function registerResources()
-    // {
-    //     $this->loadViewsFrom(__DIR__.'/../resources/views', 'login-verification');
-    // }
+    protected function registerResources()
+    {
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'login-verification');
+    }
 
-    // protected function registerRoutes()
-    // {
-    //     Route::post(
-    //         $this->app->config->get('login-verification.routes.send'),
-    //         [VerificationController::class, 'send']
-    //     )->name('login-verification.send');
+    protected function registerRoutes()
+    {
+        Route::post(
+            $this->app->config->get('login-verification.routes.send'),
+            [VerificationController::class, 'send']
+        )->name('login-verification.send');
 
-    //     Route::get(
-    //         $this->app->config->get('login-verification.routes.confirm'),
-    //         [VerificationController::class, 'confirm']
-    //     )->name('login-verification.confirm');
+        Route::get(
+            $this->app->config->get('login-verification.routes.confirm'),
+            [VerificationController::class, 'confirm']
+        )->name('login-verification.confirm');
 
-    //     Route::get(
-    //         $this->app->config->get('login-verification.routes.status'),
-    //         [VerificationController::class, 'status']
-    //     )->name('login-verification.status');
-    // }
+        Route::get(
+            $this->app->config->get('login-verification.routes.status'),
+            [VerificationController::class, 'status']
+        )->name('login-verification.status');
+    }
 
-    // protected function publishConfig()
-    // {
-    //     $this->publishes([
-    //         __DIR__.'/../config/login-verification.php' => config_path('login-verification.php'),
-    //     ], 'config');
-    // }
+    protected function publishConfig()
+    {
+        $this->publishes([
+            __DIR__.'/../config/login-verification.php' => config_path('login-verification.php'),
+        ], 'config');
+    }
 
-    // protected function publishMigrations(Filesystem $filesystem)
-    // {
-    //     $this->publishes([
-    //         __DIR__.'/../database/migrations/create_login_verifications_tables.php.stub' => $this->getMigrationFileName($filesystem),
-    //     ], 'migrations');
-    // }
+    protected function publishMigrations(Filesystem $filesystem)
+    {
+        $this->publishes([
+            __DIR__.'/../database/migrations/create_login_verifications_tables.php.stub' => $this->getMigrationFileName($filesystem),
+        ], 'migrations');
+    }
 
-    // protected function publishTranslations()
-    // {
-    //     $this->publishes([
-    //         __DIR__.'/../resources/lang' => resource_path('lang/vendor/laravel-login-verification'),
-    //     ], 'translations');
-    // }
+    protected function publishTranslations()
+    {
+        $this->publishes([
+            __DIR__.'/../resources/lang' => resource_path('lang/vendor/laravel-login-verification'),
+        ], 'translations');
+    }
 
-    // /**
-    //  * Returns existing migration file if found, else uses the current timestamp.
-    //  *
-    //  * @param  \Illuminate\Filesystem\Filesystem  $filesystem
-    //  * @return string
-    //  */
-    // protected function getMigrationFileName(Filesystem $filesystem): string
-    // {
-    //     $timestamp = date('Y_m_d_His');
+    /**
+     * Returns existing migration file if found, else uses the current timestamp.
+     *
+     * @param  \Illuminate\Filesystem\Filesystem  $filesystem
+     * @return string
+     */
+    protected function getMigrationFileName(Filesystem $filesystem): string
+    {
+        $timestamp = date('Y_m_d_His');
 
-    //     return Collection::make($this->app->databasePath().DIRECTORY_SEPARATOR.'migrations'.DIRECTORY_SEPARATOR)
-    //         ->flatMap(function ($path) use ($filesystem) {
-    //             return $filesystem->glob($path.'*_create_login_verifications_tables.php');
-    //         })->push($this->app->databasePath()."/migrations/{$timestamp}_create_login_verifications_tables.php")
-    //         ->first();
-    // }
+        return Collection::make($this->app->databasePath().DIRECTORY_SEPARATOR.'migrations'.DIRECTORY_SEPARATOR)
+            ->flatMap(function ($path) use ($filesystem) {
+                return $filesystem->glob($path.'*_create_login_verifications_tables.php');
+            })->push($this->app->databasePath()."/migrations/{$timestamp}_create_login_verifications_tables.php")
+            ->first();
+    }
 }

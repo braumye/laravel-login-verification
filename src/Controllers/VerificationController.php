98286934b56f19698b7a173bdb54aa6cf8ef6486
braumye <braum.dev@gmail.com>
2020-11-09 15:14:21 +0800

    wip

--- src/Controllers/VerificationController.php
+++ src/Controllers/VerificationController.php
@@ -52,34 +52,34 @@ class VerificationController extends Controller
     public function confirm(Request $request)
     {
         if (! $token = $request->input('token')) {
-            return $this->makeConfirmView(trans('login-verification.missing_token'));
+            return $this->makeConfirmView(trans('login-verification::login-verification.missing_token'));
         }
 
         if (! $email = $request->input('email')) {
-            return $this->makeConfirmView(trans('login-verification.missing_email'));
+            return $this->makeConfirmView(trans('login-verification::login-verification.missing_email'));
         }
 
         try {
             $token = decrypt($token);
         } catch (Throwable $e) {
-            return $this->makeConfirmView(trans('login-verification.invalid_token'));
+            return $this->makeConfirmView(trans('login-verification::login-verification.invalid_token'));
         }
 
         if (isset($token['email']) === false) {
-            return $this->makeConfirmView(trans('login-verification.invalid_token'));
+            return $this->makeConfirmView(trans('login-verification::login-verification.invalid_token'));
         }
 
         if ($token['email'] !== $email) {
-            return $this->makeConfirmView(trans('login-verification.invalid_email'));
+            return $this->makeConfirmView(trans('login-verification::login-verification.invalid_email'));
         }
 
         if (isset($token['timestamp']) === false) {
-            return $this->makeConfirmView(trans('login-verification.invalid_token'));
+            return $this->makeConfirmView(trans('login-verification::login-verification.invalid_token'));
         }
 
         $expiresIn = config('login-verification.expires_in', 3600);
         if ($token['timestamp'] + $expiresIn < time()) {
-            return $this->makeConfirmView(trans('login-verification.token_expired'));
+            return $this->makeConfirmView(trans('login-verification::login-verification.token_expired'));
         }
 
         $verification = LoginVerification::query()
@@ -88,12 +88,12 @@ class VerificationController extends Controller
             ->first();
 
         if (! $verification) {
-            return $this->makeConfirmView(trans('login-verification.invalid_token'));
+            return $this->makeConfirmView(trans('login-verification::login-verification.invalid_token'));
         }
 
         $verification->confirm();
 
-        return $this->makeConfirmView(trans('login-verification.confirm_successfully'));
+        return $this->makeConfirmView(trans('login-verification::login-verification.confirm_successfully'));
     }
 
     protected function makeConfirmView($message)
